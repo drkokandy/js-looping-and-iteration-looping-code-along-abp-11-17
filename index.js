d8fa535bc56b15714c0bf092bfbaf480361fa7d1
Dave Kokandy <drkokandy@gmail.com>
2017-11-11 04:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function printBadges(array) {
     }
   return array;
 }
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+  while (maybeTrue()) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`
+}
